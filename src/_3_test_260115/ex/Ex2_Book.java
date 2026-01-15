a8ed3929bf459270feb5a25f9684300d77f89481
lsy3709 <lsy3709@naver.com>
2026-01-15 15:25:08 +0900

    6교시, 클래스 기본 생성 실습2-2 , 매개변수 이용한 객체 생성

--- src/_3_test_260115/ex/Ex2_Book.java
+++ src/_3_test_260115/ex/Ex2_Book.java
@@ -11,6 +11,16 @@ public class Ex2_Book {
     String name;
     String price;
     String writer;
+    public Ex2_Book(){} // 기본 생성자
+    // 개선하기.
+    // 매개변수가 3개인 생성자를 이용해서, 데이터를 처음부터 담아서, 생성하기.
+    // 우클릭 -> 생성 -> 생성자 선택 -> 멤버를 다 선택. ctrl 누른 상태 선택, 클릭 하기. 생성.
+    public Ex2_Book(String name, String price, String writer) {
+        this.name = name;
+        this.price = price;
+        this.writer = writer;
+    }
+
     //기능 :책소개 메서드 이름 : introduceBook()
     void introduceBook(){
         System.out.println("책의 이름 : " + this.name);
@@ -19,4 +29,6 @@ public class Ex2_Book {
         System.out.println();
     }
     //기능 내부에는 , 현재 책의 이름과, 가격과 저자를 소개하는 출력 기능이 있습니다.
+
+
 }
--- src/_3_test_260115/ex/Ex2_BookTest.java
+++ src/_3_test_260115/ex/Ex2_BookTest.java
@@ -7,32 +7,45 @@ public class Ex2_BookTest {
         //3) 메인 메서드가 있는 클래스에서,
         //책 3권의 소개 기능을 구현 하기.
         // [클래스명] [참조형 변수이름(객체명,인스턴스라고 부름)] = new  [클래스명=생성자명]();
-        Ex2_Book book1 = new Ex2_Book();
-        Ex2_Book book2 = new Ex2_Book();
-        Ex2_Book book3 = new Ex2_Book();
 
+        // 기본 생성자 이용할 경우
+//        Ex2_Book book1 = new Ex2_Book();
+//        Ex2_Book book2 = new Ex2_Book();
+//        Ex2_Book book3 = new Ex2_Book();
+
+        // 매개변수가 3개인 생성자 이용하기.
+        Ex2_Book book1 = new Ex2_Book("샘플 책1","10000원","이상용1");
+
+        // 기본 생성자 이용할 경우
         // 책 3권에 대해서, 제목, 가격, 저자를 각각 등록.
-        book1.name = "샘플 책1";
-        book1.price = "10000원";
-        book1.writer = "이상용1";
+//        book1.name = "샘플 책1";
+//        book1.price = "10000원";
+//        book1.writer = "이상용1";
 
         // 책 소개하는 기능 사용하기. 함수 사용.
         book1.introduceBook();
 
 
-        //
+        //// 기본 생성자 이용할 경우
         // 책 3권에 대해서, 제목, 가격, 저자를 각각 등록.
-        book2.name = "샘플 책2";
-        book2.price = "10000원";
-        book2.writer = "이상용2";
+//        book2.name = "샘플 책2";
+//        book2.price = "10000원";
+//        book2.writer = "이상용2";
+
+        // 매개변수가 3개인 생성자 이용하기.
+        Ex2_Book book2 = new Ex2_Book("샘플 책2","10000원","이상용2");
 
         // 책 소개하는 기능 사용하기. 함수 사용.
         book2.introduceBook();
         //
+        // 기본 생성자 이용할 경우
         // 책 3권에 대해서, 제목, 가격, 저자를 각각 등록.
-        book3.name = "샘플 책3";
-        book3.price = "10000원";
-        book3.writer = "이상용3";
+//        book3.name = "샘플 책3";
+//        book3.price = "10000원";
+//        book3.writer = "이상용3";
+
+        // 매개변수가 3개인 생성자 이용하기.
+        Ex2_Book book3 = new Ex2_Book("샘플 책3","10000원","이상용3");
 
         // 책 소개하는 기능 사용하기. 함수 사용.
         book3.introduceBook();
